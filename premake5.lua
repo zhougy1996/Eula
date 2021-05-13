c69d9291e530c84f7710f43d24507a0e371ec518
zhougy <zhougy1996@outlook.com>
2021-05-14 02:39:58 +0800

    Use premake to genetate project

---
+++ premake5.lua
@@ -0,0 +1,88 @@
+workspace "Eula"
+    configurations { "Debug", "Release", "Dist" }
+    platforms { "Win64" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Eula"
+    location "Eula"
+    kind "SharedLib"
+    language "C++"
+    files {
+        "%{prj.location}/src/**.h", 
+        "%{prj.location}/src/**.cpp"
+        }
+
+    includedirs{
+        "Eula/vendor/spdlog/include"
+    }
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    configuration "windows"
+        cppdialect "C++17"
+        staticruntime "on"
+        systemversion "latest"
+        defines {
+            "EULA_PLATFORM_WINDOWS",
+            "EULA_BUILD_DLL"
+        }
+        postbuildcommands {
+            --Copy Eula.dll into Sandbox
+            ("{COPY} %{cfg.buildtarget.relpath} %{wks.location}bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter { "configurations:Debug" }
+        defines { "EULA_DEBUG" }
+        symbols "On"
+
+    filter { "configurations:Release" }
+         defines { "EULA_RELEASE" }
+         optimize "On"
+
+    filter { "configurations:Dist" }
+         defines { "EULA_DIST" }
+         optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    files {
+        "%{prj.location}/src/**.h", 
+        "%{prj.location}/src/**.cpp"
+        }
+
+    links{
+        "Eula"
+    }
+
+    includedirs{
+        "Eula/src",
+        "Eula/vendor/spdlog/include"
+    }
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    configuration "windows"
+        cppdialect "C++17"
+        staticruntime "on"
+        systemversion "latest"
+        defines {
+            "EULA_PLATFORM_WINDOWS"
+        }
+
+    filter { "configurations:Debug" }
+        defines { "EULA_DEBUG" }
+        symbols "On"
+
+    filter { "configurations:Release" }
+         defines { "EULA_RELEASE" }
+         optimize "On"
+
+    filter { "configurations:Dist" }
+         defines { "EULA_DIST" }
+         optimize "On"
+
